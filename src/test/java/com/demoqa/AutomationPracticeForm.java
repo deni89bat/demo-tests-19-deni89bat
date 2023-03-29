92e69c09eabc3067d1ccc991ac3d45641cbcae36
deni89bat <den.pm14@gmail.com>
2023-03-29 07:25:00 +0300

    HW2 исправление замечаний

--- src/test/java/com/demoqa/AutomationPracticeForm.java
+++ src/test/java/com/demoqa/AutomationPracticeForm.java
@@ -1,18 +1,20 @@
 package com.demoqa;
 
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.openqa.selenium.Keys.*;
-public class AutomationPracticeForm extends ConfigDemoQa {
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.CONTROL;
+
+public class AutomationPracticeForm extends TestBase {
     @Test
     void fillAndCheckAutoPracticeForm() {
         open("/automation-practice-form");
-
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Bruce");
         $("#lastName").setValue("Wayne");
         $("#userEmail").setValue("nobodyKnows@whoIm.com");
@@ -26,22 +28,16 @@ public class AutomationPracticeForm extends ConfigDemoQa {
         $(byText("Sports")).click();
         $(byText("Reading")).click();
         $(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/data/01.jpg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/01.jpg"));
         $("#currentAddress").setValue("Gotham");
         $("#state").click();
         $(byText("NCR")).click();
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
-        $(".table").shouldHave(text("Bruce Wayne"));
-        $(".table").shouldHave(text("nobodyKnows@whoIm.com"));
-        $(".table").shouldHave(text("Male"));
-        $(".table").shouldHave(text("1234567890"));
-        $(".table").shouldHave(text("01 January,1999"));
-        $(".table").shouldHave(text("Maths"));
-        $(".table").shouldHave(text("Sports, Reading, Music"));
-        $(".table").shouldHave(text("01.jpg"));
-        $(".table").shouldHave(text("Gotham"));
-        $(".table").shouldHave(text("NCR Delhi"));
+        $(".table").shouldHave(text("Bruce Wayne"), text("nobodyKnows@whoIm.com"), text("Male"),
+                text("1234567890"), text("01 January,1999"), text("Maths"), text("Sports, Reading, Music"),
+                text("01.jpg"), text("Gotham"), text("NCR Delhi"));
+
     }
 }
--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -3,12 +3,12 @@ package com.demoqa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
-public class ConfigDemoQa {
+public class TestBase {
     @BeforeAll
     static void configForDemoQa() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
     }
 }
